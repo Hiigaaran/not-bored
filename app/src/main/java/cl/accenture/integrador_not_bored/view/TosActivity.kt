52335aaf9f4edb9200a4ee005b2874a66e5522a2
Hiigaaran <simo.moralesg@gmail.com>
2022-05-30 15:52:36 -0400

    proyecto base - vistas preliminares

---
+++ app/src/main/java/cl/accenture/integrador_not_bored/view/TosActivity.kt
@@ -0,0 +1,12 @@
+package cl.accenture.integrador_not_bored.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cl.accenture.integrador_not_bored.R
+
+class TosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tos)
+    }
+}
