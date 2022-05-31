f882fda1bf795057e3719718a31028fdac8c6674
Hiigaaran <simo.moralesg@gmail.com>
2022-05-31 16:38:42 -0400

    Intent link para tosActivity generado

--- app/src/main/java/cl/accenture/integrador_not_bored/view/splash/MainActivity.kt
+++ app/src/main/java/cl/accenture/integrador_not_bored/view/splash/MainActivity.kt
@@ -1,12 +1,38 @@
 package cl.accenture.integrador_not_bored.view.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import cl.accenture.integrador_not_bored.R
+import cl.accenture.integrador_not_bored.databinding.ActivityMainBinding
+import cl.accenture.integrador_not_bored.view.activitylist.ActivityListActivity
+import cl.accenture.integrador_not_bored.view.tos.TosActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tosTxtLink.setOnClickListener {
+            onTOSLinkClicked()
+        }
+
+        binding.mainBtnStart.setOnClickListener {
+            onStartBtnClicked()
+        }
+    }
+
+    fun onTOSLinkClicked() {
+        val intent = Intent(this, TosActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onStartBtnClicked() {
+        val intent = Intent(this, ActivityListActivity::class.java).apply {
+            putExtra(R.string.main_activity_lbl_participants.toString(), binding.editMainTxtNumber.text)
+        }
+        startActivity(intent)
     }
 }
--- app/src/main/java/cl/accenture/integrador_not_bored/view/tos/TosActivity.kt
+++ app/src/main/java/cl/accenture/integrador_not_bored/view/tos/TosActivity.kt
@@ -1,12 +1,26 @@
 package cl.accenture.integrador_not_bored.view.tos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import cl.accenture.integrador_not_bored.R
+import cl.accenture.integrador_not_bored.databinding.ActivityTosBinding
+import cl.accenture.integrador_not_bored.view.splash.MainActivity
 
 class TosActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTosBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tos)
+        binding = ActivityTosBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imageButton.setOnClickListener { onReturnBtnClicked() }
+    }
+
+    fun onReturnBtnClicked() {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        }
+        startActivity(intent)
     }
 }
